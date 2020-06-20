c7a994a5bdc2caf3f95735e27d57ace757dea926
tony <ayakovlenko@users.noreply.github.com>
2020-06-20 21:01:09 +0300

    Handle dots in repository names #18 (#19)

--- git/git.go
+++ git/git.go
@@ -87,8 +87,8 @@ type RepoInfo struct {
 	Name  string
 }
 
-var ownerRepoPattern = regexp.MustCompile(`\/?(.*)\/([^.]*)(\.git)?$`)
-var repoOnlyPattern = regexp.MustCompile(`\/?([^.]*)(\.git)?$`)
+var ownerRepoPattern = regexp.MustCompile(`\/?(.*?)\/(.*?)(\.git)?$`)
+var repoOnlyPattern = regexp.MustCompile(`\/?(.*?)(\.git)?$`)
 
 // ExtractRepoInfo extracts repository information, such as the repository owner
 // (username or organization name), the repository name, and the git host of the
