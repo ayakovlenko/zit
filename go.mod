6b8e24002ee9ca09d36e8c406e0f9d5fef806f6e
tony <ayakovlenko@users.noreply.github.com>
2021-03-01 02:15:38 +0200

    chore: upgrade to Go 1.16 (#22)

--- go.mod
+++ go.mod
@@ -1,10 +1,9 @@
 module zit
 
-go 1.14
+go 1.16
 
 require (
-	github.com/google/go-jsonnet v0.15.0
-	github.com/spf13/cobra v0.0.7
-	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/whilp/git-urls v0.0.0-20191001220047-6db9661140c0
+	github.com/google/go-jsonnet v0.17.0
+	github.com/spf13/cobra v1.1.3
+	github.com/whilp/git-urls v1.0.0
 )
--- version/version.go
+++ version/version.go
@@ -2,17 +2,26 @@ package version
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
+
+	_ "embed" // embed
 )
 
-const version = "v2.2.0"
+//go:embed version.txt
+var version string
+
+// Tag returns application version tag.
+func Tag() string {
+	return strings.TrimSpace(version)
+}
 
 // VersionCmd is a command that prints the app version.
 var VersionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(version)
+		fmt.Println(Tag())
 	},
 }
