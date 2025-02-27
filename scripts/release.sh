8b4631f4eff2ac2d3c93a5a5528e994665a4f985
tony <ayakovlenko@users.noreply.github.com>
2025-02-27 09:22:46 +0200

    chore: release process improvements

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env sh
-set -eax
+set -ea
+
+# make sure the branch is main
+CURRENT_BRANCH=$(git branch --show-current)
+if [ "$CURRENT_BRANCH" != "main" ]; then
+  echo error: must be on 'main' branch
+  exit 1
+fi
 
 VERSION=$(go run cmd/zit/main.go version)
+
 git tag "$VERSION"
+
 git push origin "$VERSION"
